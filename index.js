dc0b81713ca9f43cbae99b8d4d96b267054a41d8
Mitch <mitchsiegel14@gmail.com>
2019-08-30 17:17:04 -0600

    whooo shit works now

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const jf = require("jsonfile")
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
@@ -11,8 +12,26 @@ app.get("/", function(req, res) {
 app.get("/item", function(req, res) {
   render("item", res);
 });
-app.get('/foo', (req, res) => {
-  res.render('item-template', {foo: 'FOO'});
+app.get('/items/:type/:item/:tier', (req, res) => {
+  jf.readFile("items.json",function (err, obj) {
+    if (err) console.error(err)
+    if(!obj[req.params.type][req.params.item][req.params.tier]){
+      res.end("Sorry this isn't supported yet!")
+    }else{
+    var number = req.params.tier;
+    var obj = obj[req.params.type];
+    var itemObj = obj[req.params.item];
+    var tierObj = itemObj[number];
+    var baseURL = `/items/${req.params.type}/${req.params.item}`
+    var unlockURL = tierObj.image;
+    var title = itemObj.title;
+    var itemUrl = itemObj.image;
+    var unlockName = tierObj.unlocks;
+    var type = itemObj.type;
+    res.render('item-template', {itemName: title, type: type, tNumber: number, itemUrl : itemUrl, unlockURL: unlockURL, baseURL : baseURL, unlockName: unlockName});
+    }
+  })
+
 });
 app.use(express.static('public'))
 
