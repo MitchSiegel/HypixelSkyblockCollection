8ca4a6c22203a95a6bcbf7356813499e9cae96ef
Mitch <32114214+mitchdagamer@users.noreply.github.com>
2019-09-17 09:44:39 -0600

    a lot of changes oh boy pls don't break git

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,8 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var domain = "localhost:8080"
 var urlsToCache = [
- '/', '/style.css',"https://kit.fontawesome.com/5c16ef8200.js"
+ '/','/style.css',"https://kit.fontawesome.com/5c16ef8200.js","https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/dist/js/bulma-extensions.min.js","https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/bulma-steps/dist/js/bulma-steps.min.js",
+ "https://cdn.rawgit.com/octoshrimpy/bulma-o-steps/master/bulma-steps.css","https://cdn.jsdelivr.net/npm/bulma-extensions@6.2.7/bulma-tooltip/dist/css/bulma-tooltip.min.css"
 ];
 var Farmingitems = [
   "wheat",
@@ -15,6 +16,14 @@ var images = [
   "/img/carrot.png",
   "/img/carrotminion.png",
   "/img/Clay.png",
+  "/img/Diamond_Sword.png",
+  "/img/dirt.webp",
+  "/img/ecarrot.webp",
+  "/img/ecarrotstick.webp",
+  "/img/egoldcarrot.webp",
+  "/img/Enchanted_Bread.png",
+  "/img/ebook.webp",
+  "/img/enchanted_heybale.webp",
   "/img/cm.webp"
 ]
 self.addEventListener('install', function(event) {
@@ -32,7 +41,6 @@ self.addEventListener('fetch', function(event) {
       .then(function(response) {
         // Cache hit - return response
         if (response) {
-          console.log('cached?')
           return response;
         }
         return fetch(event.request);
@@ -45,7 +53,7 @@ function loadCache(cache){
   for(x in Farmingitems){
     for(i = 0; i < 9; i++){
       cache.add(`/items/farming/${Farmingitems[x]}/${Number(i + 1)}`);
-      console.log("Added " + Farmingitems[x] + " tiers offline")
+      console.log("Saved " + Farmingitems[x] + " For offline viewing")
     }
   }
 }
